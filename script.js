1477cee884bacf833d3e841d47b98f0bb87db1e7
Suxrob9966 <rabbimovich9966@gmail.com>
2021-07-28 10:45:29 -0500

    module pattern

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*******
 // Importing module
 // import {addToCart, totalPrice as price, tq} from './shoppingCart.js'; // to cahnge the name of the variable put as and the new var name
 // addToCart('bread', 5);
@@ -12,6 +13,7 @@ ShoppingCart.addToCart('orange', 7);
 console.log(ShoppingCart.shippingCost);
 */
 
+/*****
 // import add, {addToCart, totalPrice as price, tq} from './shoppingCart.js'; // mixed imports (default and named), this is not desirable
 import add, {cart} from './shoppingCart.js';
 add('pizza', 3);
@@ -19,3 +21,36 @@ add('apple', 6);
 add('pear', 2);
 
 console.log(cart);
+*****/
+
+
+const ShoppingCart2 = (function (){ // with IFI function we create new scope and retur data just once. it is called only once, when it is created
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 232;
+    const totalQuantity = 56;
+
+    const addToCart = function(product, quantity){
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+    };
+
+    const orderStock = function(product, quantity){
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    // we need to return variables in an object and make them public API if we want to use them outside of IFE
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    }
+})();
+
+ShoppingCart2.addToCart('apple', 75);
+ShoppingCart2.addToCart('grape', 25);
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); //this is private so can't access
+
